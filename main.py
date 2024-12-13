bea5c8660265b816f5d651645259297fda3d0c5d
BialasK2000 <platypus1333@gmail.com>
2024-12-13 06:36:17 +0100

    initial commit with FastAPI conf and database connection

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///./orders.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
